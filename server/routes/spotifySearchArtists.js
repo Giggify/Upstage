ce1e9ae50ea18cbcd9671a46bfeb35fb9606e12c
Alan Jordan <alanpjordan@gmail.com>
2017-06-15 11:55:19 +1200

    Search spotify starting to hapen

--- server/routes/spotifySearchArtists.js
+++ server/routes/spotifySearchArtists.js
@@ -18,19 +18,32 @@ spotifyApi.clientCredentialsGrant()
   .then(function(data) {
     spotifyApi.setAccessToken(data.body['access_token']);
 
-// spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
-//   .then(function(data) {
-//     console.log('Artist albums', data.body);
-//   }, function(err) {
-//     console.error(err);
-//   });
-//   }, function(err) {
-//     console.log('Something went wrong!', err);
-//   });
+    // Runs a search on a str. 
+    //Need to filter and get an EXACT match in data.body.artists[i].name
+    // And return ID
+    // spotifyApi.searchArtists('Oasis')
+    //   .then(function(data) {
+    //     console.log('Search artists by "Oasis"', data.body.artists);
+    //   }, function(err) {
+    //     console.error(err);
+    //   });
 
-spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+    // Returns multiple artits?
+    // Do we need this?
+      // Get multiple artists
+  // spotifyApi.getArtists(['2hazSY4Ef3aB9ATXW7F5w3', '6J6yx1t3nwIDyPXk5xa7O8'])
+  //   .then(function(data) {
+  //     console.log('Artists information', data.body);
+  //   }, function(err) {
+  //     console.error(err);
+  //   });
+
+  // Returns top 10 tracks by an artist
+  spotifyApi.getArtistTopTracks('0oSGxfWSnnOXhD2fKuz2Gy', 'NZ')
   .then(function(data) {
-    console.log('Artist albums', data.body);
-  }, function(err) {
-    console.error(err);
+    console.log(data.body);
+    }, function(err) {
+    console.log('Something went wrong!', err);
   });
+
+  }) // Ends clientCredentialsGrant

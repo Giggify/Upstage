0bfaefd1f43aabd744dc116397df65c87e02e320
Sherly Chan <sherlyc@gmail.com>
2017-06-14 22:11:17 +1200

    removed cors from server.js because we don't need it

--- server/server.js
+++ server/server.js
@@ -1,20 +1,12 @@
 var path = require('path')
 var express = require('express')
 var bodyParser = require('body-parser')
-const cors = require('cors')
 
 var api = require('./routes/index')
 var usersDb = './db/users'
 
-const corsOptions = {
-  origin: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  credentials: true
-}
 
 var app = express()
-app.use(cors(corsOptions))
 
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../public')))

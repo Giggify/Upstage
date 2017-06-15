96829a50f186917e2fdfc51b94b2737150db887c
cici-chen <cicichen2015@qq.com>
2017-06-15 12:15:45 +1200

    added some MUI packaged

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,41 +1,69 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Field, reduxForm, formValueSelector} from 'redux-form'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {AutoComplete as MUIAutoComplete} from 'material-ui'
+import {
+  AutoComplete,
+  Checkbox,
+  DatePicker,
+  TimePicker,
+  SelectField,
+  Slider,
+  TextField,
+  Toggle,
+} from 'redux-form-material-ui';
 
 import {fetchLocations} from '../actions/locations'
 import SearchResults from './SearchResults'
 
+const renderTextField = props => (
+  <TextField hintText={props.label}
+    {...props}
+  />
+)
+
 class SearchBar extends React.Component{
-  constructor(props){
-    super(props)
-    this.state={
-      query:''
-    }
+  componentDidMount() {
+  this.refs.name // the Field
+    .getRenderedComponent() // on Field, returns ReduxFormMaterialUITextField
+    .getRenderedComponent() // on ReduxFormMaterialUITextField, returns TextField
+    .focus(); // on TextField
   }
-
-  handleChange(e){
-    this.setState(
-      {query: e.target.value}
-    )
-    this.props.dispatch(fetchLocations(e.target.value))
-  }
-
-  handleClick(e){
-    this.props.dispatch(fetchLocations(this.state.query))
-  }
-
   render(){
-
+    const {handleSubmit, reset, submitting} = this.props;
     return (
-      <div className='search-bar'>
-        <input type='search' name='search' placeholder='Search for a city..'
-          value={this.state.query}
-          onChange={this.handleChange.bind(this)}
+    <MuiThemeProvider>
+      <form className='search-bar' onSubmit={handleSubmit}>
+        <div>
+          <Field
+          name='location'
+          component={AutoComplete}
+          floatingLabelText="Search for a city.."
+          openOnFocus
+          filter={MUIAutoComplete.fuzzyFilter}
+          dataSourceConfig={{text: 'name', value: 'id'}}
+          dataSource={[
+              {id: 0, name: 'Facebook'},
+              {id: 1, name: 'Yelp'},
+              {id: 2, name: 'TV Ad'},
+              {id: 3, name: 'Friend'},
+              {id: 4, name: 'Other'},
+          ]}
           />
-        <button onClick={this.handleClick.bind(this)}>GO</button>
-        <SearchResults />
-      </div>
+        </div>
+        <div>
+          <button type='submit' disabled={submitting}>Go</button>
+          <button type='button' disabled={submitting} onClick={reset}>Clear</button>
+        </div>
+      </form>
+    </MuiThemeProvider>
     )
   }
 }
+
+SearchBar = reduxForm({
+  form: 'searchBar'  // a unique identifier for this form
+})(SearchBar)
 
 export default connect()(SearchBar)

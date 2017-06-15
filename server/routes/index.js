0d9b77772449c2d4e724abe9c2940fa69cbfc489
Sherly Chan <sherlyc@gmail.com>
2017-06-15 21:21:15 +1200

    auth login into spotify working, user is created

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,28 @@
 var express = require('express')
 var router = express.Router()
+var passport = require('passport')
 
-var usersDb = require('../db/users')
 
 router.get('/', (req, res) => {
-  let db = req.app.get('db')
-  usersDb.getUsers(db)
-    .then(users => {
-      res.json(users)
-    })
+
 })
+
+router.get('/auth',
+  passport.authenticate(
+    'spotify',
+    {scope: ['user-read-email', 'user-read-private']}
+  ),
+  function(req, res){
+   // The request will be redirected to spotify for authentication, so this
+   // function will not be called.
+});
+
+router.get('/auth/callback',
+  passport.authenticate('spotify', { failureRedirect: '/login', session: false }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+
+    res.send('success');
+  });
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
+var passport = require('./passport')
 const bodyParser = require('body-parser')
 const auth = require('./lib/auth')
 const api = require('./routes/index')
@@ -9,17 +8,20 @@ const usersDb = './db/users'
 
 const skEventSearch = require('./routes/skEvents')
 const skGetAreaID = require('./routes/skMetro')
+const index = require('./routes/index')
+
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../public')))
-app.use(passport.initialize())
-passport.use(new LocalStrategy(auth.verify))
 
-//api routes here
+passport(app)
+
+// routes here
 app.use('/api/v1/events', skEventSearch)
 app.use('/api/v1/metros', skGetAreaID)
+app.use('/', index)
 
 module.exports = (connection) => {
   app.set('connection', connection)

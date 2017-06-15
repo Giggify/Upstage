1fff84c4474f4fd46889d51acdcbba4ba481af0e
Zac Metin <zac.metin@gmail.com>
2017-06-16 10:40:31 +1200

    style date picker

--- client/components/DatePicker.jsx
+++ client/components/DatePicker.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import {orange400, orange300, orange800, grey50, grey100, grey300 ,grey400, grey700, grey900, blueGrey900} from 'material-ui/styles/colors'
 import DatePicker from 'material-ui/DatePicker'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
 const optionsStyle = {
   maxWidth: 255,
   marginRight: 'auto',
 };
+
+const muiTheme = getMuiTheme({
+ fontFamily: 'Roboto, sans-serif',
+ palette: {
+   primary1Color: orange800,
+   primary2Color: orange400,
+   primary3Color: grey400,
+   accent1Color: grey400,
+   accent2Color: grey400,
+   accent3Color: grey700,
+   labelColor: orange300,
+   textColor: grey50,
+   alternateTextColor: grey700,
+   canvasColor: grey900,
+   borderColor: grey300,
+   pickerHeaderColor: orange300,
+   shadowColor: grey900,
+ },
+});
 
 export default class DatePickerExampleToggle extends React.Component {
   constructor(props) {
@@ -13,9 +34,9 @@ export default class DatePickerExampleToggle extends React.Component {
 
     const minDate = new Date();
     const maxDate = new Date();
-    minDate.setFullYear(minDate.getFullYear() - 1);
+    minDate.setFullYear(minDate.getFullYear());
     minDate.setHours(0, 0, 0, 0);
-    maxDate.setFullYear(maxDate.getFullYear() + 1);
+    maxDate.setFullYear(maxDate.getFullYear());
     maxDate.setHours(0, 0, 0, 0);
 
     this.state = {
@@ -40,7 +61,7 @@ export default class DatePickerExampleToggle extends React.Component {
 
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
       <div>
         <div style={optionsStyle}>
           <DatePicker

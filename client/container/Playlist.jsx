cf07d73ac791893cfd30c962df6257520d3c15a3
Zac Metin <zac.metin@gmail.com>
2017-06-17 19:32:23 +1200

    toggle playlist working

--- client/container/Playlist.jsx
+++ client/container/Playlist.jsx
@@ -5,30 +5,30 @@ import Loading from 'react-loading'
 
 class Playlist extends React.Component {
 
- // constructor() {
- //   super();
- //   this.state = { show: false };
- // }
- //
- // handleClick() {
- //   this.setState({
- //     show: !this.state.show
- //   });
- // }
+ constructor() {
+   super();
+   this.state = { show: false };
+ }
+
+ handleClick() {
+   this.setState({
+     show: !this.state.show
+   });
+ }
 
 
  render() {
    return (
      <div className="Playlist">
-         {/* <button onClick={ () => this.handleClick() }>Create Playlist</button>
+         <button onClick={ () => this.handleClick() }>Toggle Playlist</button>
          <ToggleDisplay
            className="LoadPlaylist"
            show={this.props.loadplaylist}>
            <Loading type="spin" color="#ff6900" />
          </ToggleDisplay>
-       <ToggleDisplay show={this.state.show}> */}
-  <iframe src="https://open.spotify.com/embed/user/mattrainesnz/playlist/5uc9on65yKSPSiDfAYotaM" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
-       {/* </ToggleDisplay> */}
+       <ToggleDisplay show={this.state.show}>
+  <iframe src={`https://open.spotify.com/embed/user/${this.state.username}/playlist/${this.state.hashkeystuff}`} width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
+       </ToggleDisplay>
      </div>
    )
  }

d968bf93947749eb2cc90eb3bade166a1b0e0e82
cici-chen <cicichen2015@qq.com>
2017-06-19 17:51:47 +1200

    added tests for async actions/reducers

--- tests/client/actions/locations.test.js
+++ tests/client/actions/locations.test.js
@@ -1,10 +1,45 @@
 import test from 'ava'
+import nock from 'nock'
+import sinon from 'sinon'
 
 import * as actions from '../../../client/actions/locations'
+import * as helperData from '../helpers/apiSampleData'
+
+test.cb('fetchLocations dispatches correct series of actions', t=>{
+  const scope = nock('http://localhost:80')
+    .get('/api/v1/metros/city/wellington')
+    .reply(200,helperData.locations)
+
+  const dispatch = sinon.stub()
+    .onFirstCall()
+    .callsFake((action)=>{
+      console.log(action)
+      t.deepEqual(action,
+        {type:'FETCH_LOCATIONS_REQUEST',
+        message:'Searching locations...'}
+      )
+    })
+    .onSecondCall()
+    .callsFake((action) => {
+      console.log(action)
+      t.is(action.type, 'FETCH_LOCATIONS_SUCCESS')
+      t.end()
+    })
+
+  actions.fetchLocations("wellington")(dispatch)
+
+})
+
+test('filterLocation remove duplicates in results', t=>{
+  const expected=helperData.locations
+  const actual=actions.filterLocation([...helperData.locations,...helperData.locations])
+  t.deepEqual(actual,expected)
+})
 
 test('fetchLocationsRequest creates an action to indicate fetching', t => {
   const expectedAction = {
-    type: 'FETCH_LOCATIONS_REQUEST'
+    type: 'FETCH_LOCATIONS_REQUEST',
+    message:'Searching locations...'
   }
   const actual=actions.fetchLocationsRequest()
   t.deepEqual(actual,expectedAction)

a2b1eaaeb92a286240ab2fcb028d28a5c6f2c7b3
Alan Jordan <alanpjordan@gmail.com>
2017-06-19 19:55:30 +1200

    Adding in sk tests

---
+++ tests/server/routes.skEvents.test.js
@@ -0,0 +1,34 @@
+var test = require('ava')
+var request = require('supertest')
+var nock = require('nock')
+require('dotenv').config()
+
+var app = require('../../server/server')
+
+test.skip('faking passing test', t=>{
+  t.pass()
+})
+
+test.skip('API route /events/locationID returns a json', (t) => {
+  const data = {
+    resultsPage: {
+      results: {
+        event: []
+      }
+    }
+  }
+    var scope = nock('http://api.songkick.com')
+          .get(`/api/3.0/metro_areas/31455/calendar.json?apikey=${process.env.SONGKICK_API}`)
+          .reply(200, data)
+
+    request(app)
+    .get('/api/v1/events/31455')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .then((res) => {
+      scope.done()
+      t.true(res.body.hasOwnProperty('events'))
+      t.true(res.body.hasOwnProperty('artists'))
+      t.end()
+      })
+    })
